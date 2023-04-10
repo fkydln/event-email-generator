b81057c886b4126fa5a53dc829a416c97836e53a
Faz Kay <fkydln@gmail.com>
2023-04-09 22:01:52 -0700

    source variable added.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function submitForm() {
   let eventName = $("#eventName").val();
   let eventDate = $("#eventDate").val();
+  let source = $("input[name='source']:checked").val();
   let eventTier = $("#eventTier").val();
   let theSwitch = $("#theSwitch");
   let captionMax = $("#captionMax");
@@ -17,7 +18,9 @@ function submitForm() {
     eventDate +
     " / Window open time 03:00 EST<br>" +
     eventDate +
-    " / Window close time 07:00 EST (I am booking longer than needed, will goodnight the feed post event).<br>Source: hlaD05 or 06<br>Destination: FBMOTR02 or 04 <br>Notes:  From FBMOTR02/FBMOTR04 (delete which motor you do not need) I will need this routed via the standard routing below.<br>Route Program from FB Motor 02 to EEG 1 and 2  EEG 1 to CC1, CC3, CC5  EEG 2 to CC2, CC4, CC6  *****OR*****  Route Program from FB Motor 04 to EEG 3 and 4  EEG 3 to CC7, CC9, CC11  EEG 4 to CC8, CC10, CC12<br>This event will not be coming from Murats PO and will need to be invoiced to another internal client. I will bring them into the email chain shortly.<br>Thank you,";
+    " / Window close time 07:00 EST (I am booking longer than needed, will goodnight the feed post event).<br>Source: " +
+    source +
+    "<br>Destination: <br>Notes:  From FBMOTR02/FBMOTR04 (delete which motor you do not need) I will need this routed via the standard routing below.<br>Route Program from FB Motor 02 to EEG 1 and 2  EEG 1 to CC1, CC3, CC5  EEG 2 to CC2, CC4, CC6  *****OR*****  Route Program from FB Motor 04 to EEG 3 and 4  EEG 3 to CC7, CC9, CC11  EEG 4 to CC8, CC10, CC12<br>This event will not be coming from Murats PO and will need to be invoiced to another internal client. I will bring them into the email chain shortly.<br>Thank you,";
 
   // The Switch is checked:
   if (theSwitch.is(":checked")) {
